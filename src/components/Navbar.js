5b1047ba5055d312f6564daf25437f4701c6953d
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2021-10-30 00:39:04 +0330

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,23 @@
-import React,{useState} from 'react'
-import {Link} from "react-router-dom"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <>
-            <nav className="navbar">
-                <div className="navbar-container">
-<Link to="/" className="navbar-logo">
-    TRVL <i className="fab fa-typo3"></i>
-</Link>
-
-                </div>
-            </nav>
-        </>
-    )
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo">
+            TRVL <i className="fab fa-typo3"></i>
+          </Link>
+          <div className="menu-icons" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
