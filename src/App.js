2bd9d06576b420bff47e1259187e91b6a98e9ca2
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2021-10-29 23:18:31 +0330

    add logo
    add react and logo website

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
