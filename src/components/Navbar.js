ebe8bbcb8f63d70992487929b3ede6967aa0544a
shirin-nazari <74411429+shirin-nazari@users.noreply.github.com>
2021-11-03 21:50:05 +0330

    add header
    add header fo  homePage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 // import "./navbar2.css";
@@ -15,12 +15,15 @@ function Navbar() {
       setButton(true);
     }
   };
+  useEffect(() => {
+    showButton();
+  }, []);
   window.addEventListener("resize", showButton);
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             TRVL <i className="fab fa-typo3"></i>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
